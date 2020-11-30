3e4c660c9cb34ee7f09f849446dcb47075e497e6
lpjhblpj <lpjhblpj@163.com>
2020-11-30 20:09:38 +0800

    idleHandler

--- kjplayer/src/main/java/com/mic/kjplayer/WelcomeActivity.java
+++ kjplayer/src/main/java/com/mic/kjplayer/WelcomeActivity.java
@@ -3,6 +3,8 @@ package com.mic.kjplayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Looper;
+import android.os.MessageQueue;
 import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
@@ -10,41 +12,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
 
-    private static final int FUTURE =100;
-    private static final int INTERVAL =100;
+    private static final int FUTURE = 100;
+    private static final int INTERVAL = 100;
 
     private CountDownTimer countDownTimer;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // 运行时权限申请（6.0+）
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//            String[] perms = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
-//            if (checkSelfPermission(perms[0]) == PackageManager.PERMISSION_DENIED) {
-//                requestPermissions(perms, 200);
-//            }
-//        }
-        //代码执行时间统计
-//        File file = new File(Environment.getExternalStorageDirectory(),"app.trace");
-//        Debug.startMethodTracing(file.getAbsolutePath());
         //全屏设置
-        // 透明状态栏
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             getWindow().addFlags(
                     WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         }
         setContentView(R.layout.activity_welcome);
-        findViewById(R.id.iv_welcome).setOnClickListener(view -> {
-            goHome();
-        });
-        goHome();
-       // Debug.stopMethodTracing();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        queueIdle();
     }
 
     @Override
@@ -53,8 +40,19 @@ public class WelcomeActivity extends AppCompatActivity {
         cancelTimer();
     }
 
-    private void goHome(){
-         countDownTimer = new CountDownTimer(FUTURE,INTERVAL) {
+    private void queueIdle() {
+        Looper.myQueue().addIdleHandler(new MessageQueue.IdleHandler() {
+            @Override
+            public boolean queueIdle() {
+                goHome();
+                //耗时操作
+                return false;
+            }
+        });
+    }
+
+    private void goHome() {
+        countDownTimer = new CountDownTimer(FUTURE, INTERVAL) {
             @Override
             public void onTick(long l) {
 
@@ -69,8 +67,8 @@ public class WelcomeActivity extends AppCompatActivity {
         countDownTimer.start();
     }
 
-    private void cancelTimer(){
-        if (countDownTimer==null)
+    private void cancelTimer() {
+        if (countDownTimer == null)
             return;
         countDownTimer.cancel();
     }
