c5bc94b494fba7e2b42da27ebbabfa2b836fd2a9
lpjhblpj <lpjhblpj@163.com>
2021-01-07 15:20:39 +0800

    baseActivity

--- khmplayer/src/main/java/com/mic/khmplayer/base/BaseActivity.kt
+++ khmplayer/src/main/java/com/mic/khmplayer/base/BaseActivity.kt
@@ -2,20 +2,29 @@ package com.mic.khmplayer.base
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.debug
+import org.jetbrains.anko.toast
 
-abstract class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity(),AnkoLogger{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(getLayoutId())
         initListener()
         initData()
+        debug { "BaseActivity" }
     }
 
-   open abstract fun getLayoutId():Int
+    open abstract fun getLayoutId(): Int
 
-   open abstract fun initListener():Unit
+    protected fun initListener() {}
 
-   open abstract fun initData()
+    protected fun initData() {}
+
+
+    protected fun myToast(msg:String){
+        runOnUiThread { toast(msg) }
+    }
 
 }
